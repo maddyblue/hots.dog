334f66901966c51aed630c2c45d54651cc77afe0
Matt Jibson <matt.jibson@gmail.com>
2017-06-09 23:05:22 -0400

    add some URL loading; back button doesn't work yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,42 +6,64 @@ import './tachyons.min.css';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
+		var state = {
 			Builds: [],
 			Heroes: [],
 			Maps: [],
 			Modes: {},
 			Winrates: {},
 		};
+		this.params = [
+			'build',
+			'map',
+			'mode',
+		];
+		var search = new URLSearchParams(window.location.search);
+		this.params.forEach(key => {
+			if (!search.has(key)) {
+				return;
+			}
+			state[key] = search.get(key);
+		});
+		this.state = state;
 		this.handleChange = this.handleChange.bind(this);
 	}
 	componentDidMount() {
 		fetch('/api/init').then(resp => resp.json().then(data => {
-			data.build = data.Builds[0].ID;
+			if (!this.state.build) {
+				data.build = data.Builds[0].ID;
+			}
 			this.setState(data, this.update);
 		}));
 	}
-	update(state) {
-		var url = '/api/get-winrates?';
-		[
-			'build',
-			'map',
-			'mode',
-		].forEach(key => {
+	getSearch() {
+		var params = [];
+		this.params.forEach(key => {
 			var value = this.state[key];
 			if (!value) {
 				return;
 			}
-			url += '&' + key + '=' + encodeURIComponent(value);
+			value = encodeURIComponent(value)
+			params.push(key + '=' + value);
 		});
-		fetch(url).then(resp => resp.json().then(data => {
-			this.setState({Winrates: data});
+		return params.join('&');
+	}
+	update() {
+		var search = this.getSearch();
+		fetch('/api/get-winrates?' + search).then(resp => resp.json().then(data => {
+			if (this.getSearch() === search) {
+				this.setState({Winrates: data});
+			}
 		}));
 	}
 	handleChange(event) {
 		var st = {};
 		st[event.target.name] = event.target.value;
-		this.setState(st, this.update);
+		this.setState(st, () => {
+			var params = this.getSearch();
+			window.history.replaceState(null, null, '/?' + params);
+			this.update();
+		});
 	}
 	render() {
 		if (!this.state.Builds.length) {
@@ -56,16 +78,6 @@ class App extends Component {
 		modeKeys.sort().reverse();
 		var modes = modeKeys.map(k => <option key={k} value={k}>{this.state.Modes[k]}</option>);
 		modes.unshift(<option key=""></option>);
-		var winrates = Object.keys(this.state.Winrates).map(hero => {
-			var wr = this.state.Winrates[hero];
-			return (
-				<tr key={hero}>
-					<td className="pv1">{hero}</td>
-					<td>{wr.Wins}</td>
-					<td>{wr.Losses}</td>
-				</tr>
-			);
-		});
 		return (
 			<div className="sans-serif">
 				<select name="map" value={this.state.map} onChange={this.handleChange}>
@@ -78,22 +90,38 @@ class App extends Component {
 					{modes}
 				</select>
 				<div>
-					<table className="ba br2 b--black-10 pv2 ph3">
-						<thead>
-							<tr>
-								<th className="pv2 ph3">hero</th>
-								<th className="pv2 ph3">wins</th>
-								<th className="pv2 ph3">losses</th>
-							</tr>
-						</thead>
-						<tbody>
-							{winrates}
-						</tbody>
-					</table>
+					<Winrates winrates={this.state.Winrates} />
 				</div>
 			</div>
 		);
 	}
 }
+
+const Winrates = (props) => {
+	var winrates = Object.keys(props.winrates).map(hero => {
+		var wr = props.winrates[hero];
+		return (
+			<tr key={hero}>
+				<td className="pv1">{hero}</td>
+				<td>{wr.Wins}</td>
+				<td>{wr.Losses}</td>
+			</tr>
+		);
+	});
+	return (
+		<table className="ba br2 b--black-10 pv2 ph3">
+			<thead>
+				<tr>
+					<th className="pv2 ph3">hero</th>
+					<th className="pv2 ph3">wins</th>
+					<th className="pv2 ph3">losses</th>
+				</tr>
+			</thead>
+			<tbody>
+				{winrates}
+			</tbody>
+		</table>
+	);
+};
 
 export default App;
