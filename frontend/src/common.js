ce5546f1e931ec01ba264450dddce7cbbdc2e739
Matt Jibson <matt.jibson@gmail.com>
2017-10-29 17:18:58 -0400

    use localstorage instead of cookies
    fixes #12

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -148,30 +148,12 @@ function toLength(l) {
 	return mins + ':' + secs;
 }
 
-// Modified from https://www.quirksmode.org/js/cookies.html.
-function createCookie(name, value, days = 50) {
-	let expires = '';
-	if (days) {
-		const date = new Date();
-		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-		expires = '; expires=' + date.toGMTString();
-	}
-	document.cookie = name + '=' + value + expires + '; path=/';
+function createCookie(name, value) {
+	localStorage.setItem(name, value);
 }
 
 function readCookie(name) {
-	const nameEQ = name + '=';
-	const ca = document.cookie.split(';');
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) === ' ') {
-			c = c.substring(1, c.length);
-		}
-		if (c.indexOf(nameEQ) === 0) {
-			return c.substring(nameEQ.length, c.length);
-		}
-	}
-	return null;
+	return localStorage.getItem(name);
 }
 
 export {
