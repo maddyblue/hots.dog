6df159d3a2552d0e1908a88329e53affaafb1edc
Matt Jibson <matt.jibson@gmail.com>
2017-11-05 21:54:07 -0500

    uncomment extraction

--- casc/extract.sh
+++ casc/extract.sh
@@ -1,13 +1,9 @@
 CASC="/home/mjibson/src/github.com/nydus/build/bin/storm-extract -i hots"
 
-#${CASC} -t txt -x -v
-#${CASC} -t xml -x -v
+${CASC} -t txt -x -v
+${CASC} -t xml -x -v
 
-#${CASC} -t txt -f GameStrings -x -v
-
-#${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_btn_d3_ -x -v
-#${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_ui_ingame_ -x -v
-#${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_temp_ -x -v
-#${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_ui_icon -x -v
-
-#${CASC} -s mods/heroesdata.stormmod/enus.stormdata/LocalizedData/GameStrings.txt -x
+${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_btn_d3_ -x -v
+${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_ui_ingame_ -x -v
+${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_temp_ -x -v
+${CASC} -t dds -s mods/heroes.stormmod/base.stormassets/Assets/Textures -f storm_ui_icon -x -v
